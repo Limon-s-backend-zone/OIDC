70846e6a87c1ecfd433991dc15cef66f3017396f
limon-7 <liibd7@gmail.com>
2024-11-13 18:34:35 +0600

    Feat: add identity server in codebase

--- Server/IdentityServer/Configs/Config.cs
+++ Server/IdentityServer/Configs/Config.cs
@@ -1,6 +1,49 @@
-﻿namespace IdentityServer.Configs;
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace IdentityServer.Configs;
 
 public class Config
 {
+    public static IEnumerable<Client> Clients =>
+        new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client",
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                // scopes that client has access to
+                AllowedScopes = { "api1" }
+            }
+        };
     
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>
+        {
+            new ApiScope("api1", "My API")
+        };
+    public static IEnumerable<ApiResource> ApiResources =>
+        new List<ApiResource>
+        {
+           
+        };
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new List<IdentityResource>
+        {
+           
+        };
+    public static List<TestUser> TestUsers =>
+        new List<TestUser>
+        {
+           
+        };
 }
---
+++ Server/IdentityServer/Program.cs
@@ -0,0 +1,30 @@
+using IdentityServer.Configs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(Config.Clients)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddInMemoryApiResources(Config.ApiResources)
+    .AddTestUsers(Config.TestUsers)
+    .AddDeveloperSigningCredential();
+
+//
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseIdentityServer();
+// app.UseAuthorization();
+
+
+// app.MapControllers();
+
+app.Run();
